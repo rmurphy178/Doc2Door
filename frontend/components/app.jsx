2303260baf23e0076555ec8ee3dd5b9d355849a3
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 16:45:53 -0700

    create auth routes and update root app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -11,19 +11,22 @@ import {
 
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
+import { AuthRoute } from '../util/route_util';
+
 
 const App = () => (
   <div>
     <header>
-      <h1>Doc2Door</h1>
+      <Link to="/" className="header-link">
+        <h1>Doc2Door</h1>
+      </Link>
       <GreetingContainer />
     </header>
-
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
   </div>
 );
-
-
 
 export default App;
