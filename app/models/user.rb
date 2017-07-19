c7e0794565efe5d54d0ebabbbc8cc36a201a013a
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 11:50:51 -0700

    gca for git

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,7 @@ class User < ActiveRecord::Base
 
 	def self.find_by_credentials email, password
 		user = User.find_by(email: email)
-		return nil unless user
-		user.password_is?(password) ? user : nil
+    user && user.password_is?(password) ? user : nil
 	end
 
 	def password_is? password
