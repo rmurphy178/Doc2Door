e600e46c6174dc7d348c8eefe62292ec5d70cf5b
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 09:54:13 -0700

    build auth routes and, sessions controller, and users api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/create'
-
-  get 'users/update'
-
   namespace :api, defaults: { format: :json } do
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
   end
   root "static_pages#root"
 end
