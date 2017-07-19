154cd0a59492646a4f6ccff685a741c0bd193df8
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 15:28:58 -0700

    confirm session form rendering for login

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
@@ -7,6 +18,9 @@ const App = () => (
       <h1>Doc2Door</h1>
       <GreetingContainer />
     </header>
+
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
   </div>
 );
 
