196ae6fc76ae7982389983cb45e4317854dd224c
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 14:27:15 -0700

    confirm correct state shape

--- frontend/doc2door.jsx
+++ frontend/doc2door.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
-import { login , signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to Doc2Door!</h1>, root);
 });
-
-window.login = login;
-window.signup = signup;
-window.logout = logout;
