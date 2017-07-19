044e2eb44e415dead4c7d28883005387f1143d1e
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 11:21:08 -0700

    correct api controller params

--- frontend/doc2door.jsx
+++ frontend/doc2door.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { login , signup, logout } from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Doc2Door!</h1>, root);
 });
+
+
+window.login = login;
+window.signup = signup;
+window.logout = logout;
