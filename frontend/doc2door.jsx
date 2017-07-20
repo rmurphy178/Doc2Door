d2341de1800c26aa926052db3d87c5e9edc4dd76
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-19 18:03:25 -0700

    push to heroku

--- frontend/doc2door.jsx
+++ frontend/doc2door.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { login , logout } from './actions/session_actions';
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -18,14 +16,4 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
-
-  // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // TESTING END
 });
-
-
-
-window.login = login;
-window.logout = logout;
