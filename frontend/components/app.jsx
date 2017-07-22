4527157877d8c7cc4a4b517df48149720ecef20e
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-22 00:53:28 -0700

    remove extraneous container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,20 +14,14 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 import SignUpContainer from './sign_up/sign_up_container';
-import LandingContainer from './landing/landing_container';
 
 
 const App = () => (
   <div>
     <Switch>
-<<<<<<< HEAD
       <Route path="/greeting" component={GreetingContainer} />
       <AuthRoute exact path="/signup" component={SignUpContainer} />
       <AuthRoute exact path="/" component={SessionFormContainer} />
-=======
-      <AuthRoute path="/login" component={SessionFormContainer} />
-      <AuthRoute path="/signup" component={SignUpContainer} />
->>>>>>> parent of 1d6f5cf... update user model to protect against mutations when validating
     </Switch>
   </div>
 );
