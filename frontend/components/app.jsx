3f3ebd223f230d7be309f8dec8a33680232efd9b
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-22 00:44:02 -0700

    exit revert

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -20,9 +20,14 @@ import LandingContainer from './landing/landing_container';
 const App = () => (
   <div>
     <Switch>
+<<<<<<< HEAD
       <Route path="/greeting" component={GreetingContainer} />
       <AuthRoute exact path="/signup" component={SignUpContainer} />
       <AuthRoute exact path="/" component={SessionFormContainer} />
+=======
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SignUpContainer} />
+>>>>>>> parent of 1d6f5cf... update user model to protect against mutations when validating
     </Switch>
   </div>
 );
