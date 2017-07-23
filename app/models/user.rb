1dc99634d3711b3da95567cf13706a13f633306d
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-22 18:32:14 -0700

    complete model and db backend for appointments

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,16 @@ class User < ActiveRecord::Base
 	after_initialize :ensure_session_token
 	before_validation :ensure_session_token_uniqueness
 
+	has_many :appointments,
+	primary_key: :id,
+	foreign_key: :user_id,
+	class_name: :Appointment
+
+	has_many :reviews,
+	primary_key: :id,
+	foreign_key: :user_id,
+	class_name: :Review
+
 
 	def password= password
 		self.password_digest = BCrypt::Password.create(password)
