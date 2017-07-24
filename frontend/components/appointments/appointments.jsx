ff196752fa2e359df3e28906871ee8bcad735044
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-07-24 06:56:39 -0700

    add nav bar to appointments

--- frontend/components/appointments/appointments.jsx
+++ frontend/components/appointments/appointments.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 
-const Appointments = ({ currentUser }) => (
+const Appointments = ({ currentUser, logout }) => (
   <div>
+    <span className="navBar">
+      <img className="logo" src="http://res.cloudinary.com/dq5wzf090/image/upload/v1500894666/1212_rt3hls.png" />
+        <Link to="/appointments">
+          <h2 className="dashboard">Dashboard</h2>
+        </Link>
+      <button className="nav-logout" onClick={logout}>Log Out</button>
+    </span>
     <h1>Welcome to Doc2Door, {currentUser.first_name}!</h1>
   </div>
 );
