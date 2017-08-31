3eeef119a0e9d73e0f44200cc9d1357361b46d43
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-08-31 02:51:28 -0400

    add create appointment to appointment form container

--- frontend/components/appointment_form/appointment_form_container.js
+++ frontend/components/appointment_form/appointment_form_container.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { logout } from '../../actions/session_actions';
 import AppointmentForm from './appointment_form';
 import { fetchDoctors } from '../../actions/doctor_actions';
 import { fetchSpecialties } from  '../../actions/specialty_actions';
-import { fetchAppointments } from '../../actions/appointment_actions';
+import { fetchAppointments, createAppointment } from '../../actions/appointment_actions';
 
 const mapStateToProps = ({session, specialties, doctors }) => ({
   session,
@@ -13,9 +14,9 @@ const mapStateToProps = ({session, specialties, doctors }) => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout()),
   fetchDoctors: (filters) => dispatch(fetchDoctors(filters)),
-  fetchAppointments: () => dispatch(fetchAppointments())
+  fetchAppointments: () => dispatch(fetchAppointments()),
+  createAppointment: () => dispatch(createAppointment())
 });
 
 
