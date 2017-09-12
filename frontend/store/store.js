bc304ae8e57a2dbe46d24eca991dfe24ecae6a47
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-12 13:56:33 -0700

    remove unnescessary conditional from store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,21 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 import { composeWithDevTools }  from 'redux-devtools-extension';
 
-const middlewares = [thunk];
-
-if (process.env.NODE_ENV !== 'production') {
-  // must use 'require' (import only allowed at top of file)
-  const logger = require('redux-logger');
-  middlewares.push(logger);
-}
+// const middlewares = [thunk];
+//
+// if (process.env.NODE_ENV !== 'production') {
+//   const logger = require('redux-logger');
+//   middlewares.push(logger);
+// }
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(...middlewares))
+    composeWithDevTools(applyMiddleware(logger, thunk))
   )
 );
 
