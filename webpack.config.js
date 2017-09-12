78ca16951dbc663adb6570e37baf2902b275c526
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-12 13:43:57 -0700

    remove minification warning for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
 var path = require('path');
 
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = []; 
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/doc2door.jsx",
