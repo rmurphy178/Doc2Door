f090564deb90051f95baddf42829380edef72a2b
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-17 18:53:26 -0700

    create appointment requests actions

---
+++ frontend/actions/appointment_request_actions.js
@@ -0,0 +1,41 @@
+import * as APIUtil from '../util/appointment_api_util';
+
+export const RECEIVE_APPOINTMENT_REQUESTS = "RECEIVE_APPOINTMENT_REQUESTS";
+export const RECEIVE_APPOINTMENT_REQUEST = "RECEIVE_APPOINTMENT_REQUEST";
+export const RECEIVE_APPOINTMENT_REQUEST_ERRORS = "RECEIVE_APPOINTMENT_REQUEST_ERRORS";
+
+export const receiveAppointmentRequests = requests => ({
+  type: RECEIVE_APPOINTMENT_REQUESTS,
+  requests
+});
+
+export const receiveAppointmentRequest = request => ({
+  type: RECEIVE_APPOINTMENT_REQUEST,
+  request
+});
+
+export const receiveAppointmentRequestErrors = errors => ({
+  type: RECEIVE_APPOINTMENT_REQUEST_ERRORS,
+  errors
+});
+
+
+export const fetchAppointmentRequests = filters => dispatch => (
+  APIUtil.fetchAppointments(filters).then(appointments => (
+    dispatch(receiveAppointmentRequests(appointments))
+  ))
+);
+
+export const fetchAppointmentRequest = id => dispatch => (
+  APIUtil.fetchAppointment(id).then(appointment => (
+    dispatch(receiveAppointmentRequest(appointment))
+  ))
+);
+
+
+export const createAppointmentRequest = request => dispatch => (
+  APIUtil.createAppointmentRequest(request).then(result => (
+    dispatch(receiveAppointmentRequest(result))
+  ), err => (dispatch(receiveAppointmentRequestErrors(err.responseJSON))
+  ))
+);
