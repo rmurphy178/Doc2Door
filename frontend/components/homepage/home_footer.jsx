ee98e0c63e4be14d34606435feffbc00ebc94808
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-27 21:38:07 -0700

    update homepage link to linkedin profile with new custom url

--- frontend/components/homepage/home_footer.jsx
+++ frontend/components/homepage/home_footer.jsx
@@ -5,7 +5,7 @@ import { Link, withRouter, Redirect } from 'react-router-dom';
 const HomeFooter = () => (
       <div className="home-footer">
       <div className="myLinks">
-        <a href="https://www.linkedin.com/in/ryan-murphy-72a5a821/" alt="linkedin">
+        <a href="https://www.linkedin.com/in/rpm178/" alt="linkedin">
           <img className='fa linkedin' src="https://res.cloudinary.com/dq5wzf090/image/upload/v1504576536/LinkedIn-Logo-02_daj9pz.png" />
         </a>
         <a href="https://github.com/rmurphy178" alt="github">
